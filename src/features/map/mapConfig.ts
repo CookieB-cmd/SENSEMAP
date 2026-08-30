bc73972fb5346980bc52a0fb92ddc30b7f8d5e93
CookieB-cmd <stigarvehopland@gmail.com>
2026-08-30 19:29:05 +0200

    fix: use MapLibre v5 CSP worker asset

--- src/features/map/mapConfig.ts
+++ src/features/map/mapConfig.ts
@@ -1,5 +1,5 @@
 import { setWorkerUrl } from 'maplibre-gl'
-import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url'
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-csp-worker.js?url'
 import 'maplibre-gl/dist/maplibre-gl.css'
 setWorkerUrl(workerUrl)
 export const DEFAULT_CENTER:[number,number]=[5.857,61.452]
